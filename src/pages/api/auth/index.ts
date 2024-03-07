2cae3d52f3577cb065b7c6c1744fdbe28dc2e0ae
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-03-07 17:54:24 +0100

    app

--- src/pages/api/auth/index.ts
+++ src/pages/api/auth/index.ts
@@ -1,30 +1,38 @@
-import type  { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { Prisma, PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-const prisma=new PrismaClient(); //appel de prisma client pour la connexion a la base de donnée et l'execution des requetes
+
+const prisma = new PrismaClient();
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-if(req.method==='POST'){
-    const {email,mdp}=req.body;
-    try{
-        const user=await prisma.user.findUnique({
-            where:{
-                email
+    // Set CORS headers to allow requests from all origins
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'POST') {
+        const { email, mdp } = req.body;
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email }
+            });
+            if (user && bcrypt.compareSync(mdp, user.mdp)) {
+                const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                console.log(token);
+                res.setHeader('Content-Type', 'application/json');
+                res.setHeader('Cache-Control', 'max-age=180000');
+                return res.status(200).json(token);
+            } else {
+                console.log('email or password incorrect');
+                return res.status(400).json({ message: 'email or password incorrect' });
             }
-        })
-        if(user && bcrypt.compareSync(mdp,user.mdp)){
-            const token=jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:"1h"})
-            return res.status(200).json({token})
-            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'Something went wrong' });
         }
-        else{
-            return res.status(400).json({message:"email or password incorrect"})
-        }
-
+    } else {
+        // Send a response for other request methods (e.g., GET)
+        return res.status(405).json({ message: 'Method Not Allowed' });
     }
-    catch(error){
-        return res.status(500).json({message:"Something went wrong"})
-    }
-}
-
 }
